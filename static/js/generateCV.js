f323550e32009662efcc4cb45e1f45e5582e2d7c
A.J. Im <153183829+joonhaim@users.noreply.github.com>
2025-07-09 15:36:16 +0200

    Add PDF CV generator to profile page

---
+++ static/js/generateCV.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const btn = document.getElementById('download-cv');
+  if (!btn) return;
+  btn.addEventListener('click', () => {
+    const element = document.querySelector('main');
+    if (!element) return;
+    const opt = {
+      margin:       0.5,
+      filename:     'Adrien_Joon-Ha_Im_CV.pdf',
+      image:        { type: 'jpeg', quality: 0.98 },
+      html2canvas:  { scale: 2 },
+      jsPDF:        { unit: 'in', format: 'a4', orientation: 'portrait' }
+    };
+    html2pdf().set(opt).from(element).save();
+  });
+});

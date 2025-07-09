3ceaac98324ba13139dc6ee83f5d44645d8d5621
A.J. Im <153183829+joonhaim@users.noreply.github.com>
2025-07-09 15:44:56 +0200

    Improve CV PDF generation

--- static/js/generateCV.js
+++ static/js/generateCV.js
@@ -5,11 +5,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const element = document.querySelector('main');
     if (!element) return;
     const opt = {
-      margin:       0.5,
+      margin:       0.4,
       filename:     'Adrien_Joon-Ha_Im_CV.pdf',
       image:        { type: 'jpeg', quality: 0.98 },
       html2canvas:  { scale: 2 },
-      jsPDF:        { unit: 'in', format: 'a4', orientation: 'portrait' }
+      jsPDF:        { unit: 'in', format: 'a4', orientation: 'portrait' },
+      pagebreak:    { mode: ['avoid-all'] }
     };
     html2pdf().set(opt).from(element).save();
   });
